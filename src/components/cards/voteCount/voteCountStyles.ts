31cc569f96ad4777305998af7a7f669fe34ba723
Audie Leon Sheridan <audieleon@gmail.com>
2021-06-27 07:33:59 -0500

    Making it compile on limux.

--- src/components/cards/voteCount/voteCountStyles.ts
+++ src/components/cards/voteCount/voteCountStyles.ts
@@ -1,6 +1,6 @@
 import { makeStyles, createStyles } from "@material-ui/styles";
 import { Theme } from "../../../store/themeContext/themes";
-import { Props } from "./VoteCount";
+import { Props } from "./voteCount";
 
 const useStyles = makeStyles(() =>
   createStyles({
