8d2e3400f441cdceb7ea3f302032582ebd2a3e7f
Mustafa <mustafa@linumlabs.com>
2021-07-09 10:13:55 +0200

    added rpc url in env vars

--- src/wallets/connectors.ts
+++ src/wallets/connectors.ts
@@ -14,8 +14,8 @@ const POLLING_INTERVAL = 12000;
 
 // TODO: setup rpc urls
 const RPC_URLS: { [chainId: number]: string } = {
-  1: process.env.RPC_URL_1 as string,
-  4: process.env.RPC_URL_4 as string,
+  1: process.env.REACT_APP_RPC_URL_1 as string,
+  4: process.env.REACT_APP_RPC_URL_4 as string,
 };
 
 
